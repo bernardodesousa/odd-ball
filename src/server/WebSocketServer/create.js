e2985f44096d46f1fa8b7cc96723213968d32511
Bernardo SOUSA <bernardodesousa@gmail.com>
2019-04-16 20:31:10 -0300

    Implement socketServer function.

--- src/server/WebSocketServer/create.js
+++ src/server/WebSocketServer/create.js
@@ -12,3 +12,20 @@
  *    fornecida pelo evento "request" do servidor
  * @export socketServer
  */
+
+const WebSocketServer = require('websocket').server;
+const welcomePlayer = require("./welcomePlayer.js");
+
+let connections = [];
+
+function socketServer(HTTPServer) {
+    let wsServer = new WebSocketServer({
+        httpServer: HTTPServer
+    });
+
+    wsServer.on('request', connectionRequest => {
+        welcomePlayer(connections, connectionRequest);
+    });
+}
+
+module.exports = socketServer;
