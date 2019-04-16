eecdf0c1d81cb44969266f228adb695645e13abf
Bernardo SOUSA <pbs@cesar.org.br>
2019-04-16 14:24:49 -0300

    Write logPartySize description.

---
+++ src/server/WebSocketServer/logPartySize.js
@@ -0,0 +1,11 @@
+/**
+ * @import countPlayers
+ * @param connections - vetor de ponteiros para conexões de WebSocket
+ *  - Use a função countPlayers para obter o número de pessoas conectadas
+ *  - A função countPlayers recebe o vetor de conexões
+ *  - Se não houver conexões ativas, imprima "No players"
+ *  - Se houver apenas uma conexão, imprima "1 player"
+ *  - Se houver mais de uma conexão, imprima o número de conexões,
+ *    seguido de um espaço e da palavra "players", devidamente no plural
+ * @export logPartySize
+ */
