7573418ac3b9dd28f36247326406d2dc8822cdf4
Bernardo SOUSA <bernardodesousa@gmail.com>
2019-04-16 12:26:43 -0300

    _

---
+++ src/client/gameState/isPresent.js
@@ -0,0 +1,9 @@
+/**
+ * @import { getPlayers }
+ * @param id - o id do jogador
+ *  - Obter uma referência ao vetor de jogadores
+ *  - Verificar se o id recebido corresponde a um dos
+ *    jogadores do vetor de jogadores
+ *  - retornar true se sim, false se não.
+ * @export isPresent
+ */
