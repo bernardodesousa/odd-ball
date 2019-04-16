22f69e4b65df5d343e21ccb6cf73e17dc1d65180
Bernardo SOUSA <pbs@cesar.org.br>
2019-04-16 14:19:41 -0300

    Write socketServer description.

---
+++ src/server/WebSocketServer/create.js
@@ -0,0 +1,14 @@
+/**
+ * @import WebSocketServer, welcomePlayer
+ * @global connections - vetor de ponteiros para conexões de WebSocket
+ * @param HTTPServer - ponteiro para o servidor estático
+ *  - Instancie um novo WebSocket através do construtor server provido pelo 
+ *    módulo websocket, de @theturtle32
+ *  - O construtor server recebe um objeto com a propriedade "httpServer".
+ *  - Use o parâmetro recebido hTTPServer no objeto entregue ao construtor server
+ *  - Atribua a função welcomePlayer ao evento "request", emitido pelo servidor
+ *    WebSocket, através da sua função "on"
+ *  - A função welcomePlayer recebe o vetor de conexões e a requisição 
+ *    fornecida pelo evento "request" do servidor
+ * @export socketServer
+ */
