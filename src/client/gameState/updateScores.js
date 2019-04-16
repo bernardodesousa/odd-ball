497d4bb083e6490ddd8124617587ca630dbcaf59
Bernardo SOUSA <bernardodesousa@gmail.com>
2019-04-16 13:03:48 -0300

    Write updateScores description.

---
+++ src/client/gameState/updateScores.js
@@ -0,0 +1,12 @@
+/**
+ * @import getPlayers
+ * @param scores
+ *  - Obtenha uma referência para o vetor de jogadores
+ *  - Percorra o vetor de pontos recebido e atribua ao
+ *    jogador correspondente, no vetor de jogadores,
+ *    os valores de kills, deaths a alive.
+ *  - Quando um jogador estiver com o atributo alive
+ *    igual a false, adicione à seu avatar a classe CSS "dead"
+ *  - Atualize a tabela de pontos
+ * @export updateScores
+ */
