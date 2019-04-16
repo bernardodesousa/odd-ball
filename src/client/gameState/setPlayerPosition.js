e0a7eec838ff6939e4fe9483f24f16367de8d8de
Bernardo SOUSA <bernardodesousa@gmail.com>
2019-04-16 12:58:47 -0300

    Write setPlayerPosition description.

---
+++ src/client/gameState/setPlayerPosition.js
@@ -0,0 +1,10 @@
+/**
+ * @import { getPlayers, getArena }
+ * @param id
+ * @param coordinates
+ *  - Obtenha uma referência à arena no DOM
+ *  - Obtenha uma referência ao vetor de jogadores
+ *  - Se ambos os parâmetros recebidos forem definidos,
+ *    atualize a posição do avatar do jogador
+ * @export setPlayerPosition
+ */
