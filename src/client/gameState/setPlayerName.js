320b6735d778fe5ca48f0a0698d5f5cd6ce4d7fb
Bernardo SOUSA <bernardodesousa@gmail.com>
2019-04-16 12:53:15 -0300

    Add description of setPlayerName.

---
+++ src/client/gameState/setPlayerName.js
@@ -0,0 +1,11 @@
+/**
+ * @import { getPlayers }
+ * @param id
+ * @param name
+ *  - Obtenha uma referência aos jogadores
+ *  - Atualise o atributo nome
+ *      -> do objeto jogador
+ *      -> do label do jogador no DOM
+ *      -> no painel de pontos
+ * @export setPlayerName
+ */
