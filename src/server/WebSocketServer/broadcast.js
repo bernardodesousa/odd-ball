1bf894e375ddcea312bbf477abebe839e953e894
Bernardo SOUSA <bernardodesousa@gmail.com>
2019-04-16 13:15:28 -0300

    broadcast

---
+++ src/server/WebSocketServer/broadcast.js
@@ -0,0 +1,9 @@
+/**
+ * @param {*} connections - referência ao vetor de conexões
+ * @param {*} msg - a mensagem a ser transmitida para todos os jogadores
+ *  - Percorra o vetor recebido
+ *  - para cada conexão cujo atributo connected for true, chame a função send
+ *    da conexão.
+ *  - A função send recebe o objeto msg stringificado.
+ * @export broadcast
+ */
