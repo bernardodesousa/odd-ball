08be749c90993f76d0aa978e01041aff02a75268
Bernardo SOUSA <bernardodesousa@gmail.com>
2019-04-16 13:17:54 -0300

    Write countPlayers description.

---
+++ src/server/WebSocketServer/countPlayers.js
@@ -0,0 +1,8 @@
+/**
+ * 
+ * @param {*} connections - referência ao vetor de conexões
+ *  - conte quantas conexões no vetor recebido tem
+ *    o atributo connected igual a true
+ * @return {Number} total de conexões ativas
+ * @export countPlayers
+ */
