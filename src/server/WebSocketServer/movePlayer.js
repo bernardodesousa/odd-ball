33304f7a362b35bd61e575ee7846d41037740b83
bernardodesousa <bernardodesousa@gmail.com>
2019-04-18 15:13:42 -0300

    Implement a bunch of cool stuff.

--- src/server/WebSocketServer/movePlayer.js
+++ src/server/WebSocketServer/movePlayer.js
@@ -13,3 +13,20 @@
  *  - A função broadcast recebe o vetor de conexões e um objeto contendo a mensagem
  * @exports movePlayer
  */
+
+const broadcast = require('./broadcast.js');
+const GameState = require("../gameState");
+
+function movePlayer(connections, playerId, input) {
+    GameState.setCoordinates(playerId, input.coordinates);
+
+    let output = {
+        id: playerId,
+        type: "move-player",
+        coordinates: input.coordinates
+    }
+
+    broadcast(connections, output);
+}
+
+module.exports = movePlayer;
