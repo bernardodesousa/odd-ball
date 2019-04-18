b80aaa5326601571daeb1a2fcff9d4fa1a5b64dd
bernardodesousa <bernardodesousa@gmail.com>
2019-04-18 15:52:15 -0300

    Implement connection/index.

--- src/client/connection/index.js
+++ src/client/connection/index.js
@@ -11,3 +11,33 @@
  *        in which case print "Server not reachable."
  * @exports getConnection - function that returns the connection
  */
+
+import processDatagram from "./processDatagram.js";
+
+let connection;
+
+function connect () {
+    let domain = window.location.hostname;
+    let port = window.location.port;
+    connection = new WebSocket("ws://" + domain + ":" + port);
+
+    connection.onopen = () => {
+        console.log("OPEN!");
+    };
+
+    connection.onerror = (error) => {
+        console.log(error);
+    };
+
+    connection.onmessage = processDatagram;
+
+    setInterval(()=>{
+        if (connection.readyState != 1) console.log("Server not reachable.");
+    }, 5000);
+}
+
+function getConnection () {
+    return connection;
+}
+
+export { getConnection, connect };
