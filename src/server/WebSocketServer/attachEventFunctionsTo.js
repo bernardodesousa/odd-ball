351eb4813baadfc519ee207ae40f1c2c40727e99
Edson Yamamoto <edsonkazyamamoto@gmail.com>
2019-04-18 14:34:44 -0300

    adição da função de attachEventFunctionTo

--- src/server/WebSocketServer/attachEventFunctionsTo.js
+++ src/server/WebSocketServer/attachEventFunctionsTo.js
@@ -10,3 +10,21 @@
  *    tratado
  * @export attachEventFunctionsTo
  */
+
+let processDatagram = require('./processDatagram.js');
+let removePlayer = require("./removePlayer.js");
+
+function attachEventFunctionsTo(connections, playerId){
+
+    connections[playerId].on("message", (datagram)=>{
+        processDatagram(connections, playerId, datagram);
+
+    })
+    connections[playerId].on("close", ()=>{
+        removePlayer(connections, playerId)
+
+    })
+    
+}
+
+module.exports = attachEventFunctionsTo;
