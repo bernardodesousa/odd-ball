71efa4fe047f34e6b5fa3f6c8def674428cd5e42
MBdeSousa <bernardo@executa.com.br>
2019-07-02 19:18:51 -0300

    Finish countPlayers counter loop

--- src/server/WebSocketServer/countPlayers.js
+++ src/server/WebSocketServer/countPlayers.js
@@ -7,7 +7,18 @@
  */
 
 function countPlayers(connections) {
-    // TODO
+    
+    //defined counter integer for counting connections.length
+    var counter = 0;
+    //conn = integer that allocates indexes numbers
+    for (let conn = 0; conn < connections.length; conn++) {
+        //if conn index number connection is true, then it adds 1+ to the counter
+        if (connections[conn].connected) {
+            counter++;
+        }
+    }
+    //returns counter number
+    return counter;
 }
 
 module.exports = countPlayers;
