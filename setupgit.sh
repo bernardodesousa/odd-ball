8b49b15203468747972cc86f0f62890657ef56ca
prof barba <bernardodesousa@gmail.com>
2019-07-03 15:02:34 -0300

    [WIP] Add script to configure git for student on a linux machine

---
+++ setupgit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Configure Git to work with the student's GitHub account
+# at the #FatecSAT2019 NodeJS & WebSockets workshop.
+# -------------------------------------------------------
+# Usage:
+# $ setupgit <myGitHubUserName> <my@github.email>
+# -------------------------------------------------------
+
+echo "Setting up Git to work for GitHub user $1, who's email is $2"
