5ab672282f426843d95a8a847812652cf11408ec
bernardodesousa <bernardodesousa@gmail.com>
2019-07-03 16:50:09 -0300

    Write setup git script

--- setupgit.sh
+++ setupgit.sh
@@ -3,7 +3,11 @@
 # at the #FatecSAT2019 NodeJS & WebSockets workshop.
 # -------------------------------------------------------
 # Usage:
-# $ setupgit <myGitHubUserName> <my@github.email>
+# $ ./setupgit <myGitHubUserName> <my@github.email>
 # -------------------------------------------------------
 
 echo "Setting up Git to work for GitHub user $1, who's email is $2"
+git remote rename origin upstream
+git remote add origin https://github.com/$1/odd-ball.git
+git config --global user.email "$2"
+git config --global user.name "$1"
